86cfaef1641a89761ca05b2e2dbdb9a827b60e01
aryansingh186 <aryansingh700108@gmail.com>
2025-11-27 14:51:18 +0530

    cors upadte

--- Backend/server.js
+++ Backend/server.js
@@ -13,13 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-  " https://todo-list-fgwl.vercel.app",
-  "http://localhost:5173",
+    "https://todo-list-6zxa.vercel.app",
+    "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
-app.use(express.json());
 
 
 // DB Connection
