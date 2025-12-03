13991a29be64db7f4b255231fcf68fe201516957
aryansingh186 <aryansingh700108@gmail.com>
2025-12-03 20:45:05 +0530

    server

--- Backend/server.js
+++ Backend/server.js
@@ -12,9 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: [
-    "https://todo-list-dvqu.vercel.app", 
-    "http://localhost:5173" 
-  ],
+  "https://todo-list-dvqu.vercel.app",
+  "http://localhost:5173"
+],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
